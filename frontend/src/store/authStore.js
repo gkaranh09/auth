8a41391fb325f4c671e4680bd9a4dfd493880791
gkaranh09 <codewithkaran09@gmail.com>
2026-01-05 13:57:33 +0530

    frontend api access updated

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api/auth"
-    : "/api/auth";
+    : `${import.meta.env.VITE_BACKEND_URL}/api/auth`;
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set) => ({
   user: null,
