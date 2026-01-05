7cec33098e413fc18acc6e0b9cdd9ce6367a110f
gkaranh09 <codewithkaran09@gmail.com>
2026-01-05 15:14:26 +0530

    gencookie updated

--- backend/Utils/generateTokenSetCookie.js
+++ backend/Utils/generateTokenSetCookie.js
@@ -5,9 +5,8 @@ export const generateTokenSetCookie = (res, userId) => {
   });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "development",
-    secure: false,
-    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
   return token;
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,9 +1,10 @@
 import { create } from "zustand";
 import axios from "axios";
-const API_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api/auth"
-    : `${import.meta.env.VITE_BACKEND_URL}/api/auth`;
+// const API_URL =
+//   import.meta.env.MODE === "development"
+//     ? "http://localhost:5000/api/auth"
+//     : `${import.meta.env.VITE_BACKEND_URL}/api/auth`;
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api/auth`;
 axios.defaults.withCredentials = true;
 export const useAuthStore = create((set) => ({
   user: null,
@@ -62,6 +63,8 @@ export const useAuthStore = create((set) => ({
         email,
         password,
       });
+      console.log(response);
+
       set({
         isLoading: true,
         user: response.data.user,
